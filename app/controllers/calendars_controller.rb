93b7eb1075e2b7493f43996ca74aeffd5b1227aa
yupi-pi <lensyonen@gmail.com>
2023-01-17 16:40:59 +0900

    Issue1
    ハッシュロケットからシンボル型に変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,6 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+      # ハッシュロケットからシンボル型へ変更
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
